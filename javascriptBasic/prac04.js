c8d2257aad8de7de4ccdc06cd9bcabac8eb9859c
skymill2000 <skymill2000@gmail.com>
2022-02-22 13:11:20 +0900

    배열

---
+++ javascriptBasic/prac04.js
@@ -0,0 +1,10 @@
+let car01 = "sonata";
+let car02 = "bmw";
+let car03 = "tesla";
+
+let cars = [car01, car02, 392];
+console.log(cars);
+
+//in Java
+//String [] cars = new array[3];
+//ArrayList<String> stringArray = new ArrayList<String>
