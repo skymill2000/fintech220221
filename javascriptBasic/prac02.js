7c6305f9fade54c1817190b2c660614b813c441d
skymill2000 <skymill2000@gmail.com>
2022-02-22 11:29:53 +0900

    실습01 화살표 함수로 표현하기 연습

---
+++ javascriptBasic/prac02.js
@@ -0,0 +1,17 @@
+//method, func 선언 예전방식
+function test(p1, p2){
+    return p1 + p2;
+}
+
+let result = test(5,5)
+console.log(result);
+
+const plus = (p1, p2) =>{
+    return p1 + p2;
+}
+
+//work#01
+//arrow func 로 minus / multi / div 세개의 기능을 선언할것
+
+console.log(plus(1,4));
+
