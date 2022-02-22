9f962bd32a88ffa0ef4c62dbc999418fae701e27
skymill2000 <skymill2000@gmail.com>
2022-02-22 13:24:24 +0900

    for loop 배열 순회

--- javascriptBasic/prac04.js
+++ javascriptBasic/prac04.js
@@ -2,9 +2,25 @@ let car01 = "sonata";
 let car02 = "bmw";
 let car03 = "tesla";
 
-let cars = [car01, car02, 392];
-console.log(cars);
+let cars = [car01, car02, car03];
 
 //in Java
 //String [] cars = new array[3];
 //ArrayList<String> stringArray = new ArrayList<String>
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+
+for (car of cars) {
+  console.log(car);
+}
+
+for (let i = 0; i < cars.length; i++) {
+  let car = cars[i];
+  console.log(car);
+}
+
+cars.map((car) => {
+  console.error(car);
+});
