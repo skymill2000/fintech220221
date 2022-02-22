c5a64891209362d9836b56455705886766c2f952
skymill2000 <skymill2000@gmail.com>
2022-02-22 15:49:21 +0900

    state 예제

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,12 +1,19 @@
 import Welcome from "./components/Welcome";
+import { useState } from "react"; //state hook 을 import
 
 function App() {
+  let username = "미입력";
+  let [usernameWithState, setUsernameWithState] = useState("초기값");
+
+  const handleInputChange = (e) => {
+    setUsernameWithState(e.target.value);
+    console.log(username);
+  };
   return (
     <div>
-      <Welcome username="홍길동" age="22" />
-      <Welcome username="홍길동" age="22" />
-      <Welcome username="홍길동" age="22"></Welcome>
-      <Welcome username="홍길동" age="22"></Welcome>
+      이름 : <input onChange={handleInputChange}></input>
+      <h1>{usernameWithState}</h1>
+      {/* 주석처리는 ctl + / */}
     </div>
   );
 }
