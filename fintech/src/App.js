94db88ddf61640ac9fabaebef5b58bc53e1992fc
skymill2000 <skymill2000@gmail.com>
2022-02-23 10:44:03 +0900

    AxiosTest 페이지 라우터 추가 / 페이지 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListComponent from "./components/ListComponent";
+import Welcome from "./components/Welcome";
+import AxiosTest from "./pages/AxiosTest";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/test" element={<ListComponent />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/axiosTest" element={<AxiosTest />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ fintech/src/pages/AxiosTest.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderWhite from "../components/HeaderWhite";
+
+const AxiosTest = () => {
+  return (
+    <div>
+      <HeaderWhite title="Http 통신" />
+      <button>데이터 요청하기</button>
+    </div>
+  );
+};
+
+export default AxiosTest;
