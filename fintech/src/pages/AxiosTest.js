a7c308048fc1229ca356a1fdc2ef5ba76187d096
skymill2000 <skymill2000@gmail.com>
2022-02-23 11:06:40 +0900

    Axios 를 통한 http 요청 실습

--- fintech/src/pages/AxiosTest.js
+++ fintech/src/pages/AxiosTest.js
@@ -1,11 +1,24 @@
+import axios from "axios";
 import React from "react";
 import HeaderWhite from "../components/HeaderWhite";
 
 const AxiosTest = () => {
+  const handleClick = () => {
+    console.log("click");
+    axios
+      .get("https://naver.com")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <div>
       <HeaderWhite title="Http 통신" />
-      <button>데이터 요청하기</button>
+      <button onClick={handleClick}>데이터 요청하기</button>
     </div>
   );
 };
