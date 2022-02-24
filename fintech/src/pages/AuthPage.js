90c2b79d5dd2d1adc12fa4b57864a295475a7b67
skymill2000 <skymill2000@gmail.com>
2022-02-24 10:46:20 +0900

    클라이언트 ID 변경을 위한 변수 추가

--- fintech/src/pages/AuthPage.js
+++ fintech/src/pages/AuthPage.js
@@ -4,8 +4,8 @@ import HeaderWhite from "../components/HeaderWhite";
 const AuthPage = () => {
   const handleAuthBtnClick = () => {
     let tmpwindow = window.open("about:blank");
-    tmpwindow.location.href =
-      "https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0";
+    const clientId = "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF"; //본인것으로 변경해주세요 !
+    tmpwindow.location.href = `https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0`;
   };
 
   return (
