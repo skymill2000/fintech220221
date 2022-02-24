933531c5c78012a02741b10ac77255327ee23298
skymill2000 <skymill2000@gmail.com>
2022-02-24 14:24:15 +0900

    #work7 중간 힌트

--- fintech/src/pages/MainPage.js
+++ fintech/src/pages/MainPage.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import HeaderWhite from "../components/HeaderWhite";
 
@@ -12,8 +13,22 @@ const MainPage = () => {
     const accessToken = localStorage.getItem("accessToken");
     const userSeqNo = localStorage.getItem("userSeqNo");
 
+    const sendData = {};
+
+    const option = {
+      method: "GET",
+      url: "",
+      headers: {},
+      params: sendData, //object
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+      //setaccountList()
+    });
     //#work7 사용자 정보 조회 API 를 통해 accountList 에 계좌 목록을 저장 axios ; 메뉴얼 참고
     //1. axios 요청 작성
+    // get 방식 : url 데이터 지정 / data 항목대신 params 라는 항목 사용 가능
     //2. setaccountList 실행
   };
 
