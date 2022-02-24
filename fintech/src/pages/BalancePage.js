c95dc54e06d09d80211b69ada8b051c7521c8e3c
skymill2000 <skymill2000@gmail.com>
2022-02-24 16:27:41 +0900

    #work8 잔액조회 기능 실습 (중간힌트)

--- fintech/src/pages/BalancePage.js
+++ fintech/src/pages/BalancePage.js
@@ -1,9 +1,30 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 import HeaderWhite from "../components/HeaderWhite";
 
 const BalancePage = () => {
-  const getUserBalance = () => {};
+  const { fintechUseNo } = queryString.parse(useLocation().search);
+  const [balance, setbalance] = useState("");
+  console.log(fintechUseNo);
+
+  const getUserBalance = () => {
+    //   request 생성 / 응답을 text 출력
+    const accessToken = localStorage.getItem("accessToken");
+    const sendData = {};
+
+    const option = {
+      method: "",
+      url: "",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: sendData, //object
+    };
+
+    axios(option).then(({ data }) => {});
+  };
 
   return (
     <div>
