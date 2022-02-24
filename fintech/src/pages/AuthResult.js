6d2a15db1dd4c8817ce241745f4c347b3166cbf1
skymill2000 <skymill2000@gmail.com>
2022-02-24 11:53:28 +0900

    AccessToken 요청 완성

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -6,34 +6,48 @@ import axios from "axios";
 
 const AuthResult = () => {
   console.log(useLocation().search);
-  const parsedResult = queryString.parse(useLocation().search);
+  const { code } = queryString.parse(useLocation().search);
   const [accessToken, setaccessToken] = useState("토큰을 받기 전입니다.");
   const [userSeqNo, setuserSeqNo] = useState("사용자 번호");
-  console.log(parsedResult.code);
+  console.log(code);
 
   const getAccessToken = () => {
     const sendData = {
-      code: "",
-      client_id: "",
-      client_secret: "",
-      redirect_uri: "",
-      grant_type: "",
+      code: code,
+      client_id: "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF",
+      client_secret: "yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy",
+      redirect_uri: "http://localhost:3000/authResult",
+      grant_type: "authorization_code",
     };
+
+    const encodedData = queryString.stringify(sendData);
 
     const option = {
-      method: "",
-      url: "",
-      headers: {},
-      data: sendData,
+      method: "POST",
+      url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      data: encodedData,
     };
 
-    axios(option).then((response) => {});
+    axios(option).then(({ data }) => {
+      console.log(data);
+      if (data.rsp_code === "O0001") {
+        alert(data.rsp_message);
+      } else {
+        const accessToken = data.access_token;
+        const userSeqNo = data.user_seq_no;
+        setaccessToken(accessToken);
+        setuserSeqNo(userSeqNo);
+      }
+    });
   };
 
   return (
     <div>
       <HeaderWhite title={"AccessToken 요청"}></HeaderWhite>
-      <p>인증코드 : {parsedResult.code}</p>
+      <p>인증코드 : {code}</p>
       <p>AccessToken : {accessToken}</p>
       <p>사용자번호 : {userSeqNo}</p>
       <button onClick={getAccessToken}>토큰 요청</button>
