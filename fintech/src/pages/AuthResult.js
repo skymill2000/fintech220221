085a4d08c81721fe06fa793742166e1670e8184d
skymill2000 <skymill2000@gmail.com>
2022-02-24 11:28:25 +0900

    Axios 데이터 요청을 위한 sendData 및 Request Option 설정

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderWhite from "../components/HeaderWhite";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
+import axios from "axios";
 
 const AuthResult = () => {
   console.log(useLocation().search);
   const parsedResult = queryString.parse(useLocation().search);
-  console.log(parsedResult);
+  const [accessToken, setaccessToken] = useState("토큰을 받기 전입니다.");
+  const [userSeqNo, setuserSeqNo] = useState("사용자 번호");
+  console.log(parsedResult.code);
+
+  const getAccessToken = () => {
+    const sendData = {
+      code: "",
+      client_id: "",
+      client_secret: "",
+      redirect_uri: "",
+      grant_type: "",
+    };
+
+    const option = {
+      method: "",
+      url: "",
+      headers: {},
+      data: sendData,
+    };
+
+    axios(option).then((response) => {});
+  };
+
   return (
     <div>
       <HeaderWhite title={"AccessToken 요청"}></HeaderWhite>
+      <p>인증코드 : {parsedResult.code}</p>
+      <p>AccessToken : {accessToken}</p>
+      <p>사용자번호 : {userSeqNo}</p>
     </div>
   );
 };
