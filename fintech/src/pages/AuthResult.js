7640102eb797088c5d2a44db4dfec0757cafce62
skymill2000 <skymill2000@gmail.com>
2022-02-24 11:12:41 +0900

    쿼리스트링 => Js 오브젝트로 변경

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,7 +1,17 @@
 import React from "react";
+import HeaderWhite from "../components/HeaderWhite";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 const AuthResult = () => {
-  return <div>AuthResult</div>;
+  console.log(useLocation().search);
+  const parsedResult = queryString.parse(useLocation().search);
+  console.log(parsedResult);
+  return (
+    <div>
+      <HeaderWhite title={"AccessToken 요청"}></HeaderWhite>
+    </div>
+  );
 };
 
 export default AuthResult;
