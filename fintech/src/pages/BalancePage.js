b04ffb4a306c7968b6e008a8da0f861ab8af0ee5
skymill2000 <skymill2000@gmail.com>
2022-02-24 16:42:37 +0900

    #)work8 잔액 조회 API 요청 작성

--- fintech/src/pages/BalancePage.js
+++ fintech/src/pages/BalancePage.js
@@ -5,25 +5,42 @@ import queryString from "query-string";
 import HeaderWhite from "../components/HeaderWhite";
 
 const BalancePage = () => {
+  console.log(useLocation());
+  useEffect(() => {
+    getUserBalance();
+  }, []);
+
   const { fintechUseNo } = queryString.parse(useLocation().search);
   const [balance, setbalance] = useState("");
   console.log(fintechUseNo);
 
+  const genTransId = () => {
+    let countnum = Math.floor(Math.random() * 1000000000) + 1;
+    let transId = "T991599190U" + countnum; //이용기관번호 본인것 입력
+    return transId;
+  };
+
   const getUserBalance = () => {
     //   request 생성 / 응답을 text 출력
     const accessToken = localStorage.getItem("accessToken");
-    const sendData = {};
+    const sendData = {
+      bank_tran_id: genTransId(),
+      fintech_use_num: fintechUseNo,
+      tran_dtime: "20220224154030",
+    };
 
     const option = {
-      method: "",
-      url: "",
+      method: "GET",
+      url: "/v2.0/account/balance/fin_num",
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
       params: sendData, //object
     };
 
-    axios(option).then(({ data }) => {});
+    axios(option).then(({ data }) => {
+      console.log(data);
+    });
   };
 
   return (
