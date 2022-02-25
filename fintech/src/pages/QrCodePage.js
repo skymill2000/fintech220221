3b2636f61a87a8e42ae44c306da7c4d0fd7fe139
skymill2000 <skymill2000@gmail.com>
2022-02-25 11:31:58 +0900

    qrcode 생성

--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderWhite from "../components/HeaderWhite";
+const QRCode = require("qrcode.react");
 
 const QrCodePage = () => {
   return (
     <div>
       <HeaderWhite title={"QR 코드 생성"}></HeaderWhite>
+      <QRCode size={200} value="http://facebook.github.io/react/" />
     </div>
   );
 };
