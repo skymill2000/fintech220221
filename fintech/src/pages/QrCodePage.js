1fdb696dec8fbbd880abb5039510f9a31130ffe4
skymill2000 <skymill2000@gmail.com>
2022-02-25 11:40:04 +0900

    QR 코드에 finctech 번호 지정

--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import HeaderWhite from "../components/HeaderWhite";
 import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 const QRCode = require("qrcode.react");
 
@@ -12,11 +14,14 @@ const QRBlock = styled.div`
 `;
 
 const QrCodePage = () => {
+  const { search } = useLocation(); //query string get
+  const { fintechUseNo } = queryString.parse(search);
+
   return (
     <div>
       <HeaderWhite title={"QR 코드 생성"}></HeaderWhite>
       <QRBlock>
-        <QRCode size={200} value="http://facebook.github.io/react/" />
+        <QRCode size={200} value={fintechUseNo} />
       </QRBlock>
     </div>
   );
